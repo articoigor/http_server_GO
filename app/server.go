db8ef716f9b51085de5ee991eee2161adbd839fa
Igor Silva <igor-gab@hotmail.com>
2024-07-18 17:25:54 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -51,7 +51,7 @@ func main() {
 		fmt.Println(url)
 		fmt.Println(params)
 
-		if url == `localhost:4221` && params == `/` {
+		if len(params) > 1 && params == `/` {
 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		} else {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
