140c54b7797d69e0e40a3f8337969c7be2093787
Igor Silva <igor-gab@hotmail.com>
2024-07-18 15:39:58 -0300

    add: return 200 status

--- app/server.go
+++ app/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -23,5 +25,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	arr := make([]byte, 128)
+
+	bytes, err := conn.Read(arr)
+
+	req := string(bytes)
+
+	if err == nil {
+		splitReq := strings.Split(req, "\r\n")
+
+		re, _ := regexp.Compile(`(GET|POST)\s(.*)\s+`)
+
+		details := re.FindString(splitReq[0])
+
+		path := strings.Split(details, " ")[1]
+
+		if path == "http://localhost:4221/abcdefg" {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+
+		if path == "http://localhost:4221" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		}
+	}
 }
