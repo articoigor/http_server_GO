08ae2a12dd1664c30893ca9ddb29f6e7bd4ffac2
Igor Silva <igor-gab@hotmail.com>
2024-07-19 01:50:48 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -110,7 +110,7 @@ func checkEcho(components []string, params string, regex *regexp.Regexp, conn ne
 	if echo != "" {
 		bodyRegex, _ := regexp.Compile("/")
 
-		encoder, encodedBody := "", bodyRegex.Split(echo, -1)[2]
+		encoder, encodedBody := "", bodyRegex.Split(echo, -1)[1]
 
 		if len(components) >= 3 {
 			encoder = regex.Split(components[2], -1)[1]
