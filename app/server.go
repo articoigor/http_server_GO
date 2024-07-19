715b41dd8acb5afc76b58de13bac5f9902eaf174
Igor Silva <igor-gab@hotmail.com>
2024-07-18 23:38:40 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -73,7 +73,7 @@ func processPostRequest(components []string, params string, conn net.Conn) {
 
 	fileName := fileNameRegex.Split(params, -1)[1]
 
-	fmt.Println(fileNameRegex.Split(params, -1)[0])
+	fmt.Println(fileNameRegex.Split(params, -1)[1])
 
 	directory := fmt.Sprintf("/tmp/data/codecrafters.io/http-server-tester/%s", fileName)
 
