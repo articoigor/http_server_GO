6ffdf85bc89c6929915393c157cb0831926e83b7
Igor Silva <igor-gab@hotmail.com>
2024-07-19 01:21:24 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -122,7 +122,7 @@ func checkEcho(components []string, params string, regex *regexp.Regexp, conn ne
 			str = fmt.Sprintf("Content-Encoding: %s\r\n", encoder)
 		}
 
-		str += fmt.Sprintf("Content-Encoding: %s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(encodedBody), encodedBody)
+		str += fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(encodedBody), encodedBody)
 
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n" + str))
 
